d6e868fe294d6467e0d3b10b787a38c5293f8546
AhhhhYe <hiddendreams3112@gmail.com>
2024-02-05 10:13:12 +0800

    Added Player shooting

---
+++ 07_Source_Project/Assets/Script/Player_Spawn_Bullets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Spawn_Bullets : MonoBehaviour
+{
+    public GameObject Bullet1;
+    bool shoot;
+    private float ShootCD = .2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootCD -= Time.deltaTime;
+        if (ShootCD <= 0)
+        {
+            Firing();
+            ShootCD = .2f;
+        }
+    }
+    void Firing()
+    {
+        Debug.Log("Shoot Once");
+        Instantiate(Bullet1, transform.position, transform.rotation);
+        Bullet1.transform.position += new Vector3(0, 1);
+        
+    }
+}
