674e4d9c782f6330356897620a3492d8be981e8a
AhhhhYe <hiddendreams3112@gmail.com>
2024-02-06 10:03:16 +0800

    Added Player Healthbar
    used slider to perform action

---
+++ 07_Source_Project/Assets/Script/PlayerHealthBarScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBarScript : MonoBehaviour
+{
+    public Slider HpSlider;
+    public void SetMaxHealth(int health)
+    {
+        HpSlider.maxValue = health;
+        HpSlider.value = health;
+    }
+    public void SetHealth(int health)
+    {
+        HpSlider.value = health;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 07_Source_Project/Assets/Script/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth = 4;
+    public int currentHealth;
+    public PlayerHealthBarScript healthBarScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBarScript.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakenDamage(1);
+        }
+    }
+    void TakenDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBarScript.SetHealth(currentHealth);
+    }
+}
