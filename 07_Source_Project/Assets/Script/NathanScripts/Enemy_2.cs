241c78b51d51df144d1835840b05a8d6d4f7fb4e
Nathan <limtenathan2003@gmail.com>
2024-02-06 11:43:55 +0800

    Nathan 1.3
    update check

---
+++ 07_Source_Project/Assets/Script/NathanScripts/Enemy_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_2 : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        Vector2 movement = Vector2.down * speed * Time.deltaTime;
+        transform.Translate(movement);
+        if (transform.position.y < -5.2f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Projectiles")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
