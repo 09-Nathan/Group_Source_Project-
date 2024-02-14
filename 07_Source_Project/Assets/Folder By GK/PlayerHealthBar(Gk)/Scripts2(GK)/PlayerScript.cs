30e3a15f879442a26ac226c647e079056dfad4b6
09-Nathan <limtenathan2003@gmail.com>
2024-02-14 22:13:49 +0800

    nathan 1.7

---
+++ 07_Source_Project/Assets/Folder By GK/PlayerHealthBar(Gk)/Scripts2(GK)/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth = 4;
+    public int currentHealth;
+    public PlayerHealthBarScript healthBarScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBarScript.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakenDamage(1);
+        }
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void TakenDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBarScript.SetHealth(currentHealth);
+    }
+}
---
+++ 07_Source_Project/Assets/Folder By GK/ShooterFolder(GK)/ShooterScripts(GK)/ShooterScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    public GameObject objecttoInstitate;
+    bool shoot;
+    private float ShootCD = .3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootCD -= Time.deltaTime;
+        if (ShootCD <= 0)
+        {
+            shooting();
+            ShootCD = .3f;
+        }
+    }
+    void shooting()
+    {
+        Instantiate(objecttoInstitate,transform.position,transform.rotation);
+
+    }
+}
